b34523d231dc667ade01024cc344e4354fa57280
Azzaid <johanas.azzaid@gmail.com>
2017-11-06 15:44:25 +0300

    Card game is done

--- Tasks/games/Mach-mach/scripts/gameEngineMK2.js
+++ Tasks/games/Mach-mach/scripts/gameEngineMK2.js
@@ -1,25 +1,24 @@
 var flippedCount = 0;
 var lastFlipped = null;
-var backSideNumber = 0;
+var backSideNumber = 1;
+var pairsLeft = 0;
+var startTime =0;
 
 function nextBackSide() {
     if (backSideNumber < 3) {
         backSideNumber++;
-        document.getElementById('sample').style.backgroundPosition = (144*backSideNumber + "px " + 0 + "px");
     } else {
-        backSideNumber=0;
-        document.getElementById('sample').style.backgroundPosition = (144*backSideNumber + "px " + 0 + "px");
+        backSideNumber=1;
     }
-
+    document.getElementById('sample').src = ("images/backs/" + backSideNumber + ".jpg");
 }
 function prevBackSide() {
-    if (backSideNumber > 0) {
+    if (backSideNumber > 1) {
         backSideNumber+=(-1);
-        document.getElementById('sample').style.backgroundPosition = (144*backSideNumber + "px " + 0 + "px");
     } else {
         backSideNumber=3;
-        document.getElementById('sample').style.backgroundPosition = (144*backSideNumber + "px " + 0 + "px");
     }
+    document.getElementById('sample').src = ("images/backs/" + backSideNumber + ".jpg");
 }
 
 function CreateCard (place, cardtyphe) {
@@ -27,13 +26,16 @@ function CreateCard (place, cardtyphe) {
     const card = document.createElement('div');
     card.className = "card";
     
-    const backSide = document.createElement('div');
+    const backSide = document.createElement('img');
         backSide.className = "card_back";
-        backSide.style.backgroundPosition = (144*backSideNumber + "px " + 0 + "px");
+        backSide.src = ("images/backs/" + backSideNumber + ".jpg");
 
-    const frontSide = document.createElement('div');
+
+
+    const frontSide = document.createElement('img');
         frontSide.className = "card_face";
-        frontSide.style.backgroundPosition = ((144*(cardtyphe%4)) + "px " + (194*Math.floor(cardtyphe/4)) + "px");
+        //frontSide.style.backgroundPosition = ((149*(cardtyphe%6)) + "px " + (227*Math.floor(cardtyphe/6)) + "px");
+        frontSide.src = ("images/cards/" + cardtyphe + ".jpg");
 
     card.appendChild(backSide);
     card.appendChild(frontSide);
@@ -47,10 +49,9 @@ function addLeadinZero(i) {
     return i;
 }
 
-function countTime(startTime) {
+function countTime() {
     var now = Math.floor(Date.now() / 1000);
     var diff = now - startTime;
-    console.debug(diff);
     var m = Math.floor(diff / 60);
     var s = Math.floor(diff % 60);
     m = addLeadinZero(m);
@@ -62,20 +63,33 @@ function countTime(startTime) {
 
 function startgame () {
 
-    var cardQuantity = document.getElementById('difficulty').value;
-    var startTime = Math.floor(Date.now() / 1000);
-    setInterval(countTime(startTime), 500);
+    hideWinScreen();
 
-    document.getElementById('ruleset').style.display = "none";
+    var cardQuantity = document.getElementById('difficulty').value;
+    pairsLeft = +cardQuantity;
+
+    const cardHolder = document.getElementById('cardholder');
+
+    startTime = Math.floor(Date.now() / 1000);
+    setInterval(countTime, 500);
+
+
+
+    //document.getElementById('ruleset').style.display = "none";
+
+    for (;cardHolder.childNodes.length > 0;) {
+        var child = cardHolder.childNodes[0];
+        cardHolder.removeChild(child);
+    }
 
     var freeSlots = [];
-    for (var i = 0; i < cardQuantity; i++) {
+    for (var i = 0; i < cardQuantity*2; i++) {
         freeSlots.push(i);
     }
 
-    var shuffledCards = Array(cardQuantity);
-    for ( i = 0; i < cardQuantity; i++) {
-        cardtyphe = Math.floor(Math.random()*52);
+    var shuffledCards = Array(cardQuantity*2);
+    for (var i = 0; i < cardQuantity; i++) {
+        cardtyphe = Math.floor(Math.random()*70);
         randomIndex = Math.floor(Math.random()*freeSlots.length);
         placeForCard1 = freeSlots[randomIndex];
         freeSlots.splice(randomIndex, 1);
@@ -87,7 +101,7 @@ function startgame () {
         shuffledCards[placeForCard2] = CreateCard(placeForCard2, cardtyphe);
     }
 
-    for ( i = 0; i < cardQuantity; i++) {
+    for (var i = 0; i < cardQuantity*2; i++) {
         document.getElementById('cardholder').appendChild(shuffledCards[i])
     }
     const clearfix = document.createElement('div');
@@ -96,31 +110,44 @@ function startgame () {
 }
 
 function flipCard() {
-    if (flippedCount) {
+    if (flippedCount == 1) {
+        flippedCount++;
         if (lastFlipped.rankAndSuit == this.rankAndSuit && lastFlipped!==this){
             this.classList.add('flipped');
-
-            wanquish = (item) => {
-                item.style.visibility = 'hidden';
-                lastFlipped.style.visibility = 'hidden';
-                flippedCount = 0;
-            }
-            setTimeout(wanquish(this), 1000);
+            var item = this;
+            setTimeout(function () {wanquish(item)}, 1000);
 
         } else {
             this.classList.add('flipped');
-
-            backFlip = (item) => {
-                this.classList.remove('flipped');
-                lastFlipped.classList.remove('flipped');
-                flippedCount = 0
-            }
-
-            setTimeout(backFlip(this), 3000);
+            var item = this;
+            setTimeout(function () {backFlip(item)}, 1000);
         }
-    } else {
+    } else if (flippedCount == 0) {
         this.classList.add('flipped');
         lastFlipped = this;
         flippedCount++;
     }
+}
+
+function backFlip(item) {
+    item.classList.remove('flipped');
+    lastFlipped.classList.remove('flipped');
+    flippedCount = 0
+}
+
+function wanquish(item) {
+    item.style.visibility = 'hidden';
+    lastFlipped.style.visibility = 'hidden';
+    flippedCount = 0;
+    pairsLeft+=(-1);
+    if (pairsLeft == 0) {
+        showWinScreen();
+    }
+}
+
+function showWinScreen() {
+    document.getElementById('winScreen').style.display = "block";
+}
+function hideWinScreen() {
+    document.getElementById('winScreen').style.display = "none";
 }
