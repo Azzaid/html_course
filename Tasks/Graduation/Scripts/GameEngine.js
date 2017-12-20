55d8855da57f847dae178d4bd9002d36b91b6ffc
Azzaid <johanas.azzaid@gmail.com>
2017-12-21 02:49:22 +0300

    Power meter, programmable actuators, and some physik engine tweaks.

--- Tasks/Graduation/Scripts/GameEngine.js
+++ Tasks/Graduation/Scripts/GameEngine.js
@@ -6,8 +6,6 @@ function kd(text,smth) {
 
 const jointWith = 60;
 const jointHeight = 60;
-const boneLenght = 30;
-const actuatorLenght =30;
 const gravity = 1;
 const friction = 1;
 const groundLevel = 600;
@@ -17,33 +15,47 @@ const boneHeigth = 20;
 const gameSpeed = 0.5;                 //delay in ms between frames
 const minForceToRound = 2;
 const actuatorMaxForce = 300;
-const boneSpringiness = 2;
-const frictionForce = 200;
+const boneSpringiness = 4;
 const debugMode = true;
 
 
 function createVectorFromXY (projectionXY) {
     let lenght = Math.sqrt(Math.pow(projectionXY[0],2) + Math.pow(projectionXY[1],2));
-    let angle = Math.atan(projectionXY[1]/projectionXY[0])*57.29;
+    let angle = Math.sign(projectionXY[1])*90;
+    if (projectionXY[0] != 0) {
+        angle = Math.atan(projectionXY[1]/projectionXY[0])*57.29;
+    };
     if (projectionXY[0]<0) {angle = ((-1) * Math.sign(angle) * 180) + angle};
-    kd("will create from " + projectionXY + " and got " + [lenght, angle]);
+    //kd("will create vector from " + projectionXY + " and got " + [lenght, angle]);
     return ([lenght, angle])
 }
 
 function createXYFromVector (vector) {
-    return ([vector[0]*Math.cos(vector[1]/57.29), vector[0]*Math.sin(vector[1]/57.29)])
+    let x = vector[0]*Math.cos(vector[1]/57.29);
+    let y = vector[0]*Math.sin(vector[1]/57.29);
+    //kd("will create XY from " + vector + " and got " + x + " " + y);
+    return ([x,y])
 }
 
 function projectVectorOnAngle (vector, angle) {
     let projectedVector = [vector[0]*Math.cos((vector[1] - angle)/57.29), angle];
+    //kd("will project " + vector + " on angle " + angle + " and got " + projectedVector);
     return (projectedVector)
 }
 
 function getAngleBetweenDots(dot1,dot2) {
     let x = dot2[0] - dot1[0] + 1;
     let y = dot2[1] - dot1[1] + 1;
-    let angle = Math.atan(y/x)*57.29;
-    if (x<0) {angle = ((-1) * Math.sign(angle) * 180) + angle};
+    let angle = Math.sign(y)*90;
+    if (x != 0) {
+        angle = Math.atan(y/x)*57.29;
+        //kd("raw angle is " + angle);
+    };
+    if (x<0) {
+        angle = ((-1) * Math.sign(angle) * 180) + angle;
+        //kd("but x < 0 so now angle is " + angle);
+    };
+    //kd("will create angle from " + dot1 + " and " + dot2 + " got x " + x + " and y " + y + " so angle is " + angle);
     return angle;
 }
 
@@ -160,6 +172,16 @@ class Joint {
         this.precalcedSpeed[0] += (this.resultForce[0])/2;
         this.precalcedSpeed[1] += (this.resultForce[1])/2;
 
+        if (this.grounded && this.precalcedSpeed[1]>0) {
+            this.precalcedSpeed[1] = 0;
+            if (Math.abs(this.precalcedSpeed[0]) > (this.resultForce[1]*friction)) {
+                this.precalcedSpeed[0] -= Math.sign(this.resultForce[0])*this.resultForce[1]*friction;
+                kd(this.domId + " speed after friction is " + this.precalcedSpeed[0]);
+            } else {
+                this.precalcedSpeed[0] = 0;
+            };
+        };
+
         kd(this.domId + " result speed will be ", this.precalcedSpeed);
     }
 
@@ -170,26 +192,20 @@ class Joint {
         this.speed[0] += (this.resultForce[0]*timeFactor)/2;
         this.speed[1] += (this.resultForce[1]*timeFactor)/2;
 
-        if ((this.currentY + (this.speed[1] * timeFactor) + 5) >= groundLevel) {
-            this.speed=[0,0];
-            this.currentY = groundLevel;
-            if (this.grounded){
-                if (Math.abs(this.resultForce[0]) > frictionForce) {
-                    this.currentX += ((this.resultForce[0] - Math.sign(this.resultForce[0])*frictionForce)*timeFactor)/2
-                }
+        if (this.grounded && this.speed[1]>0) {
+                this.speed[1] = 0;
+            if (Math.abs(this.speed[0]) > this.resultForce[0]*friction) {
+                this.speed[0] -= ((Math.sign(this.resultForce[0])*this.resultForce[1]*friction)*timeFactor);
             } else {
-                this.currentX += this.speed[0] * timeFactor;
+                this.speed[0] = 0;
+            };
+        } else if ((this.currentY + (this.speed[1] * timeFactor) + 5) >= groundLevel) {
+                this.speed[1]= (groundLevel - this.currentY)/timeFactor;
                 this.grounded = true;
-                this.domObj.classList.add("joint__grounded");
-            }
-        } else {
-            this.grounded = false;
-            this.currentX += this.speed[0] * timeFactor;
-            this.currentY += this.speed[1] * timeFactor;
-            this.domObj.classList.remove("joint__grounded");
-        }
+    };
 
-
+    this.currentX += this.speed[0]*timeFactor;
+    this.currentY += this.speed[1]*timeFactor;
 
         kd(this.domId + " RESULT speedX " + this.speed[1]);
         kd(this.domId + " move to", [this.currentX, this.currentY]);
@@ -219,9 +235,13 @@ class Joint {
         })
     };
 
-    addForce(sourceId, force) {
+    addForce(sourceId, force, passList) {
         let inforce = force;
+        let forbiddenConnections = passList.slice();
         if ((Math.abs(inforce[0]) > minForceToRound) || (Math.abs(inforce[1]) > minForceToRound)) {
+
+            forbiddenConnections.push(this.domId);
+
             if (Math.abs(inforce[0]) < minForceToRound) {
                 inforce[0] = 0};
             if (Math.abs(inforce[1]) < minForceToRound) {
@@ -233,10 +253,28 @@ class Joint {
 
             kd(this.domId + " result force is", this.resultForce);
 
+            this.forceSpreadCoefficient = [0,0];
+            this.forceSumm = [0,0];
+
             this.connectedBoneAndJointPares.forEach((boneJointPair) => {
-                if (!(sourceId == boneJointPair[0].domId) && !(sourceId == boneJointPair[1].domId)) {
+                if (!(forbiddenConnections.includes(boneJointPair[0].domId)) && !(forbiddenConnections.includes(boneJointPair[1].domId))) {
+                    let coefficientForThisJointX =  createXYFromVector(projectVectorOnAngle([1,0],getAngleBetweenDots([this.currentX,this.currentY],[boneJointPair[1].currentX,boneJointPair[1].currentY])));
+                    let coefficientForThisJointY =  createXYFromVector(projectVectorOnAngle([1,90],getAngleBetweenDots([this.currentX,this.currentY],[boneJointPair[1].currentX,boneJointPair[1].currentY])));
+                    this.forceSumm[0] += Math.abs(coefficientForThisJointX);
+                    this.forceSumm[1] += Math.abs(coefficientForThisJointY);
+                }
+            });
+
+            this.forceSpreadCoefficient[0] = 1/this.forceSumm[0];
+            this.forceSpreadCoefficient[1] = 1/this.forceSumm[1];
+
+            this.connectedBoneAndJointPares.forEach((boneJointPair) => {
+                if (!(forbiddenConnections.includes(boneJointPair[0].domId)) && !(forbiddenConnections.includes(boneJointPair[1].domId))) {
                     kd(this.domId + " pass force to " + boneJointPair[1].domId, inforce);
-                    boneJointPair[1].addForce(this.domId, createXYFromVector(projectVectorOnAngle(createVectorFromXY(inforce), getAngleBetweenDots([this.currentX,this.currentY],[boneJointPair[1].currentX,boneJointPair[1].currentY]))));
+                    let passedForce = createXYFromVector(projectVectorOnAngle(createVectorFromXY(inforce), getAngleBetweenDots([this.currentX,this.currentY],[boneJointPair[1].currentX,boneJointPair[1].currentY])));
+                    passedForce[0] = passedForce[0]*this.forceSpreadCoefficient[0];
+                    passedForce[1] = passedForce[1]*this.forceSpreadCoefficient[1];
+                    boneJointPair[1].addForce(this.domId, passedForce, forbiddenConnections);
                 }
             })
         }
@@ -245,37 +283,12 @@ class Joint {
 
     addGravity() {
         kd(this.domId + " gravity", jointWeight * gravity);
-        this.addForce("gravity", [0, jointWeight * gravity]);
+        this.addForce("gravity", [0, jointWeight * gravity], []);
     }
 
     addGroundReaction() {
-        if (this.grounded && this.resultForce[1] > 0) {
-
-            let groundReactionForce = [0, 0];
-
-                groundReactionForce[1] = -this.resultForce[1];
-
-            if (Math.abs(this.resultForce[0]) > this.resultForce[1] * friction) {
-                groundReactionForce[0] = -Math.sign(this.resultForce[0]) * this.resultForce[1] * friction;
-                this.frictionForceLeft = 0;
-            } else {
-                groundReactionForce[0] = -this.resultForce[0];
-                this.frictionForceLeft = (Math.sign(this.resultForce[0]) * this.resultForce[1] * friction) - this.resultForce[0];
-                this.frictionForceDirection = -Math.sign(this.resultForce[0]);
-                kd(this.domId + " left friction " + this.frictionForceLeft + " pointed to " + this.frictionForceDirection)
-            }
-
-            kd(this.domId + " got ground reaction ", groundReactionForce);
-
-            this.addForce("ground", groundReactionForce);
-        }
-    }
-
-    addForceToStop(timeFactor) {
-        if (this.freshlyGroundedFlag == 1) {
-            kd(this.domId + " speed was ", this.speed);
-            kd(this.domId + " vertically stopped by ", [0,-this.speed[1]/timeFactor]);
-            this.addForce("ground", [0,-this.speed[1]/timeFactor])
+        if (this.grounded) {
+            this.addForce("ground", [0, -this.resultForce[1]], []);
         }
     }
 }
@@ -333,23 +346,26 @@ class Bone {
         });
     }
 
-    addForce(sourceId, force) {
+    addForce(sourceId, force, passList) {
         let inforce = force;
+        let forbiddenConnections = passList.slice();
         if ((Math.abs(inforce[0])>minForceToRound) || (Math.abs(inforce[1])>minForceToRound)) {
+            forbiddenConnections.push(this.domId);
             if (Math.abs(inforce[0])<minForceToRound) {inforce[0]=0};
             if (Math.abs(inforce[1])<minForceToRound) {inforce[1]=0};
             kd(this.domId + " got force from " + sourceId, inforce);
             this.joints.forEach((joint)=>{
-                if (!(sourceId == joint.domId)) {
+                kd(this.domId + " trying to add force to " + joint.domId + " but passlist is ", forbiddenConnections);
+                if (!(forbiddenConnections.includes(joint.domId))) {
                     kd(this.domId + " pass force to "+joint.domId, inforce);
-                    joint.addForce(this.domId, inforce);
+                    joint.addForce(this.domId, inforce, forbiddenConnections);
                 }
             })
         }
     }
     addGravity() {
         kd(this.domId + " gravity", this.lenght*boneWeight*gravity);
-        this.addForce("gravity", [0,this.lenght*boneWeight*gravity]);
+        this.addForce("gravity", [0,this.lenght*boneWeight*gravity], []);
     }
 
     addBoneSpringiness () {
@@ -367,13 +383,13 @@ class Bone {
         if (springinessForce !== 0) {
             if (( this.joints[0].grounded && !this.joints[1].grounded ) || ( !this.joints[0].grounded && this.joints[1].grounded )) {
                 if (this.joints[0].grounded) {
-                    this.joints[1].addForce(this.domId, createXYFromVector([springinessForce*2,this.angle]));
+                    this.joints[1].addForce(this.domId, createXYFromVector([springinessForce*2,this.angle]), [this.domId]);
                 } else {
-                    this.joints[0].addForce(this.domId, createXYFromVector([springinessForce*2,this.angle]));
+                    this.joints[0].addForce(this.domId, createXYFromVector([springinessForce*2,this.angle+180]), [this.domId]);
                 }
             } else {
-                this.joints[0].addForce(this.domId, createXYFromVector([springinessForce,this.angle]));
-                this.joints[1].addForce(this.domId, createXYFromVector([springinessForce,this.angle]));
+                this.joints[0].addForce(this.domId, createXYFromVector([springinessForce,this.angle+180]), [this.domId]);
+                this.joints[1].addForce(this.domId, createXYFromVector([springinessForce,this.angle]), [this.domId]);
             }
         }
     }
@@ -440,21 +456,28 @@ class Actuator {
         this.domStopTimeField.title = "stop time";
         this.domProgrammBox.appendChild(this.domStopTimeField);
 
+        this.addBarButton = document.createElement("div");
+        this.addBarButton.classList.add("button");
+        this.addBarButton.textContent = "add step";
+        this.addBarButton.id = "barAdd"+this.domId;
+        this.addBarButton.onclick = function (event) {
+            game.addElement(event);
+        };
+        this.domProgrammBox.appendChild(this.addBarButton);
+
         this.stepProgramBars.push([this.domForceSlider, this.domStartTimeField, this.domStopTimeField]);
     }
 
     createProgram () {
-        this.stepProgramBars.forEach(function (bar) {
+        this.stepProgramBars.forEach((bar)=>{
             this.steps.push([bar[0].value,bar[1].value,bar[2].value])
-        })
+        });
     }
 
     move(timeFactor) {
         kd("move actuator", this.domId);
         this.lenght = Math.sqrt(Math.pow((this.bones[0].middleY-this.bones[1].middleY) ,2) + Math.pow((this.bones[0].middleX-this.bones[1].middleX),2));
         this.angle = getAngleBetweenDots([this.bones[0].middleX,this.bones[0].middleY],[this.bones[1].middleX,this.bones[1].middleY]);
-        //this.angle = Math.atan((this.bones[0].middleY-this.bones[1].middleY)/(this.bones[0].middleX-this.bones[1].middleX))*57.29;
-        if ((this.bones[0].middleX-this.bones[1].middleX)>0) {this.angle -= 180;}
 
         this.domObj.style.transition = timeFactor + "s";
 
@@ -474,15 +497,18 @@ class Actuator {
 
     act(time) {
         this.actuatorCurrentForce = 0;
-        for (step of this.steps) {
+        kd(this.domId + " will wait for time " + time);
+        for (var step of this.steps) {
             if (step[1]<=time && step[2]>=time) {
                 this.actuatorCurrentForce = step[0];
             }
         }
         if (this.actuatorCurrentForce !== 0) {
-            this.bones[0].addForce(this.domId, createXYFromVector([-this.actuatorCurrentForce,this.angle]));
-            this.bones[1].addForce(this.domId, createXYFromVector([this.actuatorCurrentForce,this.angle]));
+            kd(this.domId + " is acting with force " + this.actuatorCurrentForce);
+            this.bones[0].addForce(this.domId, createXYFromVector([-this.actuatorCurrentForce,this.angle]), []);
+            this.bones[1].addForce(this.domId, createXYFromVector([this.actuatorCurrentForce,this.angle]), []);
         }
+        return(this.actuatorCurrentForce);
     }
 }
 
@@ -498,7 +524,9 @@ class Creature {
         this.lifetime = 0;
         this.interval = 0.5;
         this.moveing = false;
-        this.newJointsGrounded = 0;
+        this.energySpend = 0;
+        this.actuatorsPower = 0;
+        this.powerMeter = document.getElementById("powerMeter")
     }
 
     addElement(ev) {
@@ -536,7 +564,18 @@ class Creature {
         }
     }
 
+    addElementByClick(event) {
+        this.actuators[event.target.id.slice(-1)].addStepProgramBar();
+    }
+
     calcForces() {
+        this.actuatorsPower = 0;
+
+        kd("program actuators", this.actuators);
+        this.actuators.forEach(function (actuator) {
+            actuator.createProgram();
+        });
+
         kd("add gravity to", this.bones);
         this.bones.forEach(function (bone) {
             bone.addGravity();
@@ -547,10 +586,15 @@ class Creature {
             joint.addGravity();
         });
 
-        kd("force from muscle", this.muscles);
-        this.actuators.forEach(function (actuator) {
-            actuator.addForce(this.lifetime);
+        kd("force from actuators", this.actuators);
+        this.actuators.forEach((actuator)=>{
+            this.actuatorsPower += actuator.act(this.lifetime);
         });
+
+        //kd("add ground reaction", this.joints);
+        //this.joints.forEach(function (joint) {
+            //joint.addGroundReaction();
+        //});
 
         kd("predict movement of ", this.joints);
         this.joints.forEach(function (joint) {
@@ -561,11 +605,6 @@ class Creature {
         this.bones.forEach(function (bone) {
             bone.addBoneSpringiness();
         });
-
-        //kd("add ground reaction", this.joints);
-        //this.joints.forEach(function (joint) {
-            //joint.addGroundReaction();
-        //});
     }
 
     countTimeInterval() {
@@ -600,15 +639,19 @@ class Creature {
 
     step() {
 
-
         this.calcForces();
 
         this.countTimeInterval();
 
-        kd("move all", this.joints);
+        this.lifetime += this.interval;
+        this.energySpend += this.actuatorsPower*this.interval;
+
+            kd("move all", this.joints);
         for (let joint of this.joints) {
             joint.move(this.interval);
         }
+
+        this.powerMeter.textContent = "Power spend is " + this.energySpend + " kVA";
     }
 
     starMove(){
@@ -631,6 +674,8 @@ class Creature {
     }
 
     reset() {
+        this.lifetime = 0;
+        this.energySpend = 0;
         this.joints.forEach(function (joint) {
             joint.reset();
         });
@@ -656,7 +701,7 @@ class Gamefield {
     }
 
     stopSim(){
-        this.creature.stopMove();
+        this.creature.reset();
     }
 
     takeElement(ev) {
@@ -666,6 +711,10 @@ class Gamefield {
     allowDrop(ev) {
         ev.preventDefault();
     }
+    addElement(ev){
+        this.creature.addElementByClick(ev);
+    }
+
     addElement2(ev){
         kd("called from", ev.target);
         kd("adding element to creature from gamefeild", this.creature);
